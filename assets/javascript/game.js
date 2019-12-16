e91a1e0a75a9205d41a1faff9980927303992b62
hallieraew <hallieweintraub@gmail.com>
2019-12-16 08:13:26 -0600

    pseudo

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// array with all the options
+// random selector for the compuer choice
+// variables for wins
+// variables for losses
+// variables for guesses left
+//variables to references in the html for display
+// key press function
+    //determine which key
+    //if the key was correct - action
+    //if the key was not correct - action
+//display user choice
+//display computer choice
+//diplay win
+//display loss
+//display tie
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var loss = 0;
+var ties = 0;
+var guesses = 15;
+
